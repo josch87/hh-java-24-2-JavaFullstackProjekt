6a74ba09339049f835cae68151b2f7d2133bd956
Aljoscha Zöller <mail@aljoschazoeller.com>
2024-04-19 17:00:03 +0200

    Add ASCII Art greeting and update dependencies
    This update introduces the use of jfiglet to display an ASCII Art greeting, "Hello World". It includes the addition of jfiglet as a new library dependency in the pom.xml file. Also, a new .idea/vcs.xml file has been added for VCS directory mappings.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello World");
+
+		String asciiArt = FigletFont.convertOneLine("Hello World");
+		System.out.println(asciiArt);
 	}
 }
